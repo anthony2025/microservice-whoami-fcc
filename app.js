de5fa591a094fa5ca225d0faa7a88bf87bc8a110
Anthony Ascencio <anthony2025@gmail.com>
2016-11-11 23:29:17 -0500

    finally working

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const PORT = process.env.PORT || 4000
 const app = require('express')()
 
+const ip = require("request-ip")
+const accepts = require('accepts')
+const useragent = require('express-useragent')
+
+app.use(ip.mw())
+app.use(useragent.express())
+
 app.get('/', function(req, res){
     let response = {
-      ipaddress: req.ip.slice,
-      language: req.headers["accept-language"].slice(0,5),
-      software: null
+      ipaddress: req.clientIp,
+      language: accepts(req).languages()[0],
+      software: req.useragent.platform + ', ' + req.useragent.os
     }
-    res.send(response);
+    res.send(response)
 });
 
 app.listen(PORT, () => console.log('App running on port: ' + PORT))
