6a4923365a1c2f9dee0cb554a276395a3b4a4f68
Anthony Ascencio <anthony2025@gmail.com>
2016-11-11 23:10:05 -0500

    ip and language

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const PORT = process.env.PORT || 4000
 const app = require('express')()
 
 app.get('/', function(req, res){
-    let response = {}
-    res.json(req.ip);
+    let response = {
+      ipaddress: req.ip.slice,
+      language: req.headers["accept-language"].slice(0,5),
+      software: null
+    }
+    res.send(response);
 });
 
 app.listen(PORT, () => console.log('App running on port: ' + PORT))
