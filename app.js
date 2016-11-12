8fd4fdf32dd8bb228227a0f219315c367733690a
Anthony Ascencio <anthony2025@gmail.com>
2016-11-11 22:58:35 -0500

    trying a simpler approach to get the ip address

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require('express')()
 
 app.get('/', function(req, res){
     let response = {}
-    res.send(req.connection.remoteAddress);
+    res.json(req.ip);
 });
 
 app.listen(PORT, () => console.log('App running on port: ' + PORT))
